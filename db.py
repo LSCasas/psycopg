761089381df4ab0cb62e0aecf235bc62989aeb8c
Luis Casas <luisscasas45@gmail.com>
2025-04-23 13:30:46 -0600

    FEAT/ add select funciton

---
+++ db.py
@@ -0,0 +1,15 @@
+import psycopg
+
+CONN_STRING = "dbname=dvdrental user=postgres password=postgres host=localhost"
+
+def select(columns, table, where=None):
+    with psycopg.connect(CONN_STRING) as conn:
+        with conn.cursor() as cur:
+            column_list = ", ".join(columns)
+            
+            
+            where_string = f"WHERE {where[0]} {where[1]} {where[2]}" if where else ""
+            query = f"SELECT {column_list} FROM {table} {where_string};"
+            cur.execute(query)
+            result = cur.fetchall()
+    return result
--- main.py
+++ main.py
@@ -5,25 +5,26 @@ import os
 # Load variables from the .env
 load_dotenv()
 
-
 # Read variables
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_HOST = os.getenv("DB_HOST")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT", "5432")  
+DB_PORT = os.getenv("DB_PORT", "5432")
 
-# Build connection
-conn_str = f"dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={DB_PORT}"
+# Build connection string
+conn_str = f"dbname={DB_NAME} user={DB_USER} host={DB_HOST} port={DB_PORT} password={DB_PASSWORD}"
 
+# Establish the connection
 with psycopg.connect(conn_str) as connection:
     print("successful connection")
 
+    # Use the connection and cursor inside the with block
     with connection.cursor() as cursor:
         cursor.execute(
-            "INSERT INTO test (num, data) VALUES (%s, %s)", 
+            "INSERT INTO test (num, data) VALUES (%s, %s)",
             (100, "any")
         )
-       
+        # Commit the transaction within the context
+        connection.commit()
 
-connection.commit()
