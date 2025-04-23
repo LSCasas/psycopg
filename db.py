506b5a2beb6d9b124090a7a7c0b7de7d6bcdabb6
Luis Casas <luisscasas45@gmail.com>
2025-04-23 13:31:32 -0600

    FEAT/ add delete funciton

--- db.py
+++ db.py
@@ -13,3 +13,17 @@ def select(columns, table, where=None):
             cur.execute(query)
             result = cur.fetchall()
     return result
+
+def delete(table, where=None):
+    conn = psycopg.connect(CONN_STRING)
+
+    
+    with conn.cursor() as cur:
+        where_string = f"WHERE {where[0]} {where[1]} {where[2]}" if where else ""
+        query = f"DELETE FROM {table} {where_string};"
+        cur.execute(query)
+        result = cur.fetchall()  
+    
+    cur.execute()
+    conn.close()
+    return result
